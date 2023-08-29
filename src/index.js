50eb88b4f9616552606c5caec658aa0e8dcdcd14
Matt Eva <matthew.eva@flatironschool.com>
2023-08-29 08:22:01 -0700

    saving basic global aside styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.js";
+import "./index.css"
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<RouterProvider router={router} />)
