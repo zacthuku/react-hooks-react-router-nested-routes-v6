e74ec8e95b196dc7a1c13c569624fd49fed6e252
Matt Eva <matthew.eva@flatironschool.com>
2023-10-12 14:30:45 -0700

    updated starting code files - added db.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,23 @@
-import users from "../data";
+import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard";
 import NavBar from "../components/NavBar";
 
 function Home() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/users")
+      .then(r => r.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error))
+  }, [])
   
-  const userList = users.map(user =><UserCard key={user.id} user={user}/>)
+  const userList = users.map(user =>{
+    return <UserCard key={user.id} user={user}/>
+  });
 
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>Home!</h1>
         {userList}
